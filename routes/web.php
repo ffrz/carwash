8c8848417ab7127ef127ba4e9020eaa939a6f813
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-03-31 16:00:19 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin/dashboard', [DashboardController::class, 'index']);
+
