f94a8293df770b315fb7de7f86ad53ab94dc88b6
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-04-01 23:30:41 +0700

    nonaktifkan card header

--- resources/views/admin/_components/card-header.blade.php
+++ resources/views/admin/_components/card-header.blade.php
@@ -1,8 +1,8 @@
-<div class="card-header">
-  <div class="card-title">
-    <h4 class="mb-0">{{ $title }}</h4>
-    @if (!empty($description))
+{{-- @if (!empty($description))
+  <div class="card-header">
+    <div class="card-title">
+      <h4 class="mb-0">{{ $title }}</h4>
       <p class="text-muted my-2">{{ $description }}</p>
-    @endif
+    </div>
   </div>
-</div>
+@endif --}}
